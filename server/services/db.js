b9a9def2a64c2e8e07208b2a74df730cebdeca66
Matthew Randle <randle.matthew@hotmail.co.uk>
2019-03-15 10:49:02 +0000

    Updated db.js to add ssl if in production

--- server/services/db.js
+++ server/services/db.js
@@ -1,12 +1,31 @@
 const mysql = require("mysql");
-const keys = require("../../config/keys");;
+const keys = require("../../config/keys");
+const fs = require("fs");
 
-var pool = mysql.createPool({
-    host: keys.databaseHost,
-    user: keys.databaseUser,
-    password: keys.databasePassword,
-    database: keys.database,
-    multipleStatements: true
-});
+var pool;
+
+if(process.env.NODE_ENV === "development") {
+    pool = mysql.createPool({
+        host: keys.databaseHost,
+        user: keys.databaseUser,
+        password: keys.databasePassword,
+        database: keys.database,
+        multipleStatements: true
+    });
+}
+else {
+    pool = mysql.createPool({
+        host: keys.databaseHost,
+        user: keys.databaseUser,
+        password: keys.databasePassword,
+        database: keys.database,
+        multipleStatements: true,
+        ssl: {
+            ca: fs.readFileSync(keys.mysqlCA),
+            key: fs.readFileSync(keys.mysqlKey),
+            cert: fs.readFileSync(keys.mysqlCert)
+        }
+    });
+}
 
 module.exports = pool;
