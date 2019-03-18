76af9b5d17b6c73de5d1c71781eb1e616eb2d812
Matthew Randle <randle.matthew@hotmail.co.uk>
2019-03-18 10:19:41 +0000

    Testing our setTimeout for links

--- components/home/index.js
+++ components/home/index.js
@@ -1,27 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Link from "next/link";
-import { withRouter } from "next/link";
 
 import Home from "./Home";
 
 export class HomeContainer extends Component {
-    componentDidMount() {
-        this.props.router.prefetch("/courses");
-    }
-
     getLatestCourses() {
         let latest = this.props.course.latestCourses.map((item, i) => {
             return (
                 <div className="home_course" key={i}>
-                    <Link href={`/course/${item.url}`}>
-                        <a>
+                        <a onClick={(() => setTimeout(() => this.props.router.push(`/course/${item.url}`), 100))}>
                             <img
                                 className="home_course_image"
                                 src={item.image}
                             />
                         </a>
-                    </Link>
                 </div>
             );
         });
@@ -42,4 +34,4 @@ function mapStateToProps({ course, article }) {
     return { course, article };
 }
 
-export default withRouter(connect(mapStateToProps)(HomeContainer));
+export default connect(mapStateToProps)(HomeContainer);
