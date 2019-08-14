c19cfe4a87f7b191356af9e975050c3d74d2b6b5
Matthew Randle <exportknowledge.matthew@gmail.com>
2019-08-14 09:45:15 +0100

    Removed homeloader

--- pages/howmanyhours.js
+++ pages/howmanyhours.js
@@ -1,12 +1,10 @@
 import React from "react";
-import dynamic from "next/dynamic";
 import Head from "next/head";
 
 import "../stylesheets/css/HowManyHours.css";
 
 import ErrorBoundary from "../components/ErrorBoundary";
 import initialSetupFetch from "../utils/initialSetupFetch";
-import HomeLoader from "../components/home/HomeLoader";
 import HowManyHours from "../components/howmanyhours";
 
 const HowManyHoursPage = () => {
